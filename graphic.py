91ebd07bebbc031dbe367e515ffe4c25b5bdabf7
Jherfson Castro <jherfson.castro@gmail.com>
2020-07-23 12:44:05 -0300

    Update graphic.py

--- graphic.py
+++ graphic.py
@@ -9,72 +9,62 @@ z_image_txt = np.loadtxt(path, delimiter=" ", usecols=(2))
 alpha = 0.01
 c = 0.1
 r = 0.1
+max_itera = 100
+step_tolerance = 0.001
 
 def angular_frequency(frequency):
     return 2 * np.pi * frequency
 
 
-def y_hat(f, c, r):
-    z_real = r * (1+ (angular_frequency(f) * c * r)**2)**-1 
-    z_image = - (angular_frequency(f) * r**2 * c * (1 + (angular_frequency(f) * r * c)**2)**-1)
+def y_hat(frequency, c, r):
+    z_real = r * (1+ (angular_frequency(frequency) * c * r)**2)**-1 
+    z_image = - (angular_frequency(frequency) * r**2 * c * (1 + (angular_frequency(frequency) * r * c)**2)**-1)
     return z_real, z_image
 
 
-def plot_line(z_real_txt, z_image_txt, c, r):
-    x_values = [i for i in range(int(min(z_real_txt)) - 1, int(max(z_real_txt)) + 2)]
-    y_values_z_real = [y_hat(f, c, r) for f in x_values]
-    plt.plot(x_values,y_values_z_real, 'r')
-    plt.plot(z_real_txt, z_image_txt, 'bo')
-
-
-def MSE(z_real_txt, z_image_txt):
-    custo = 0
-    m = float(len(frequency))
-    for i in range(0, len(frequency)):
-        # custo = custo + (z_real_txt[i] - z_image_txt[i])**2
-        custo += (z_real_txt[i] - z_image_txt[i])**2
-    print("custo: ",custo)
-        
-    return custo/m
-
-def gradient_descent_step(c, r, z_real_txt , z_image_txt, alpha):
+def gradient_descent(c, r, z_real_txt ,z_image_txt, alpha):
     
-    erro_c = 0
-    erro_r = 0
-    m = float(len(frequency))
-    for i in range(0,len(z_real_txt)):
+    step_c = 0
+    step_r = 0
+    for i in range(max_itera):
         W=1
-        
+        print(f'Frequência: {frequency[i]}')
+
         denominador = (1 + (angular_frequency(frequency[i]) * r * c)**2)**2 
         # capacitor
         numerador_c = (-2 * r**3 * angular_frequency(frequency[i])**2 * c)
         sc_1 = 2 * W * (y_hat(frequency[i] ,c, r)[0] - z_real_txt[i]) * numerador_c/denominador
         sc_2 = 2 * W * (y_hat(frequency[i], c, r)[1] - z_image_txt[i]) * (-angular_frequency(frequency[i]) * r**2*((-angular_frequency(frequency[i])* r * c)**2 + 1))/denominador
-        
+        sc = sc_1 + sc_2
         # Resitor
         numerador_r = (-(angular_frequency(frequency[i]) * c * r)**2 + 1)
         sr_1 = 2 * W * (y_hat(frequency[i], c, r)[0] - z_real_txt[i]) * numerador_r/denominador
         sr_2 = 2 * W * (y_hat(frequency[i], c, r)[1] - z_image_txt[i]) * (-2 * angular_frequency(frequency[i]) * r * c)/denominador 
+        sr = sr_1 + sr_2
 
+        step_c = alpha * sc        
+        c -= step_c
+        print(f'Novo C:{c}')
 
-        erro_c += sc_1 + sc_2
-        erro_r += sr_1 + sr_2
+        step_r = alpha * sr
+        r -= step_r
+        print(f'Novo R:{r}')
+
+        if abs((step_c <= step_tolerance and step_r <= step_tolerance)):
+            break
+
+    return c, r
+
+# epoch = 30
+
+# def gradient_descent(c, r, z_real_txt, z_image_txt, alpha, epoch):
+#     custo = np.zeros(epoch)
+#     for i in range(epoch):
+#         c , r = gradient_descent_step(c, r, z_real_txt, z_image_txt, alpha)
+#         custo[i] = MSE(z_real_txt, z_image_txt)
         
-    new_c = c - alpha * (1/m) * erro_c
-    new_r = r - alpha * (1/m) * erro_r
-
-    return new_c, new_r
-
-epoch = 30
-
-def gradient_descent(c, r, z_real_txt, z_image_txt, alpha, epoch):
-    custo = np.zeros(epoch)
-    for i in range(epoch):
-        c , r = gradient_descent_step(c, r, z_real_txt, z_image_txt, alpha)
-        custo[i] = MSE(z_real_txt, z_image_txt)
-        
-    return c, r, custo
+#     return c, r, custo
 
 
 
-print(gradient_descent(c, r, z_real_txt, z_real_txt, alpha, epoch))
+print(gradient_descent(c, r, z_real_txt, z_image_txt, alpha))
